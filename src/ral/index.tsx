d858d5111ae6961a573f5d569de555a017c7f11a
huangziyi01 <huangziyi01@baidu.com>
2016-04-22 10:25:04 +0800

    quick push

--- src/ral/index.tsx
+++ src/ral/index.tsx
@@ -1,4 +1,4 @@
-import * as yog from 'yog2-kernel'
+//import * as yog from 'yog2-kernel'
 import * as _ from 'lodash'
 
 // export default async(settings:any) => {
@@ -21,21 +21,5 @@ import * as _ from 'lodash'
 //     return await yog.ralP('service', _.defaultsDeep(settings, defaults))
 // }
 export default (settings:any) => {
-    var defaults = {
-        headers: {
-            'x_bd_product': yog.log.req.headers.x_bd_product,
-            'x_bd_subsys': yog.log.req.headers.x_bd_subsys,
-            'x_bd_logid': yog.log.req.headers.x_bd_logid,
-            'x_bd_userip': yog.log.req.headers.x_bd_userip,
-            'x_bd_routerip': yog.log.req.headers.x_bd_routerip,
-            'x-forwarded-for': yog.log.req['headers.x-forwarded-for'],
-            'x_bd_idc': yog.log.req.headers.x_bd_idc,
-            'x_bd_dna': yog.log.req.headers.x_bd_dna,
-            'user-agent': yog.log.req.headers['user-agent'],
-            'bfe-atk': yog.log.req.headers['bfe-atk'],
-            'bfe_logid': yog.log.req.headers.bfe_logid,
-            'bfeip': yog.log.req.headers.bfeip
-        }
-    }
     return {}
 }
