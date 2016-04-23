121a90805d5f3aa4cc52b7b19c763c0b308aa48f
huangziyi01 <huangziyi01@baidu.com>
2016-04-23 16:57:40 +0800

    quick push

--- src/fetch/index.tsx
+++ src/fetch/index.tsx
@@ -1,19 +1,13 @@
 import * as process from 'process'
 import * as axios from 'axios'
 
-let basename:string = ''
-
 export interface Option {
     url:string
     method:string
     data?:any
 }
 
-export const setBasename = (_basename:string)=> {
-    basename = _basename
-}
-
-export default (option:Option)=> {
+export default (basename:string)=>(option:Option)=> {
     if (process.browser) {
         return axios.post(basename + option.url, {
             method: option.method,
