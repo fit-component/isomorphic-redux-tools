8e1d47a4e2cf46d321175274d519289b8d6b0407
huangziyi01 <huangziyi01@baidu.com>
2016-04-27 13:55:35 +0800

    quick push

--- src/fetch/index.tsx
+++ src/fetch/index.tsx
@@ -2,6 +2,7 @@ import * as process from 'process'
 import * as axios from 'axios'
 
 export interface Option {
+    type:string
     url:string
     method:string
     data?:any
@@ -19,7 +20,7 @@ export const setServerRender = (render)=> {
     serverRender = render
 }
 
-export default (action,option:Option)=> {
+export default (option:Option)=> {
     let promise:any = null
     if (process.browser) {
         promise = axios({
@@ -37,7 +38,7 @@ export default (action,option:Option)=> {
         }
     }
     return {
-        type: action,
+        type: option.type,
         promise: promise
     }
 }
