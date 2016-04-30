d7b0c90665e4bab8b65ca9b8f06b7ddca8d981f1
huangziyi01 <huangziyi01@baidu.com>
2016-04-30 12:21:06 +0800

    quick push

--- src/promise-middleware/index.tsx
+++ src/promise-middleware/index.tsx
@@ -26,35 +26,41 @@ export default (store:any) => (next:any) => (action:any) => {
     // 不符合约定 promise 规范
     if (!promise) return next(action)
 
-    // 符合规范,但不是 promise
-    if (typeof promise.then !== 'function') {
-        return next(action)
-    }
-
     const REQUEST = type + '_REQUEST'
     const SUCCESS = type + '_SUCCESS'
     const FAILURE = type + '_FAILURE'
 
-    next(extendRest(rest, {
-        type: REQUEST
-    }))
-    
-    return promise.then((req:any) => {
-        let data = {}
-        if (process.browser) {
-            data = req.data
-        } else {
-            data = req
-        }
-        next(extendRest(rest, {
-            data: data, type: SUCCESS
+    if (typeof promise.then !== 'function') {
+        // 符合规范,但不是 promise
+        // action:请求成功
+        return next(extendRest(rest, {
+            type: SUCCESS
         }))
-        return true
-    }).catch((error:any) => {
+    } else {
+        // action:准备发送请求
         next(extendRest(rest, {
-            error, type: FAILURE
+            type: REQUEST
         }))
-        console.log('PromiseMiddleware error:', error)
-        return false
-    })
+
+        return promise.then((req:any) => {
+            let data = {}
+            if (process.browser) {
+                data = req.data
+            } else {
+                data = req
+            }
+            // action:请求成功
+            next(extendRest(rest, {
+                data: data, type: SUCCESS
+            }))
+            return true
+        }).catch((error:any) => {
+            // action:请求失败
+            next(extendRest(rest, {
+                error, type: FAILURE
+            }))
+            console.log('PromiseMiddleware error:', error)
+            return false
+        })
+    }
 }
