e202e5b3ece2f4f0bb1b5d0d877cb79e7a831bf5
huangziyi01 <huangziyi01@baidu.com>
2016-05-27 18:51:54 +0800

    quick push

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,13 @@
 /// <reference path="../../../../typings-module/yog2-kernel.d.ts" />
 /// <reference path="../../../../typings-module/process.d.ts" />
 /// <reference path="../../../../typings-module/redux-immutablejs.d.ts" />
+//
+import connect from './connect'
+import promiseMiddleware from './promise-middleware'
+import serverRender from './server-render'
+import fetch from './fetch'
+import router from './router'
+import store from './store'
+import * as service from './service'
+
+export {connect, promiseMiddleware, serverRender, fetch, router, store, service}
