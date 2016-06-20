5a8024ff3ba23631ed984783f2e28c92e817a5a8
huangziyi01 <huangziyi01@baidu.com>
2016-06-20 19:12:20 +0800

    quick push

--- src/promise-middleware/index.tsx
+++ src/promise-middleware/index.tsx
@@ -2,28 +2,28 @@
 
 import * as process from 'process'
 
-const getRest:any = (action:any) => {
-    let rest:any = {}
-    let restKeys:any = Object.keys(action).filter((key:string) => {
+const getRest: any = (action: any) => {
+    let rest: any = {}
+    let restKeys: any = Object.keys(action).filter((key: string) => {
         return key !== 'promise' && key !== 'type'
     })
-    restKeys.forEach((key:string) => {
+    restKeys.forEach((key: string) => {
         rest[key] = action[key]
     })
     return rest
 }
 
-const extendRest:any = (rest:any, extend:any) => {
-    let all:any = rest
-    Object.keys(extend).forEach((extendkey:string) => {
+const extendRest: any = (rest: any, extend: any) => {
+    let all: any = rest
+    Object.keys(extend).forEach((extendkey: string) => {
         all[extendkey] = extend[extendkey]
     })
     return all
 }
 
-export default (store:any) => (next:any) => (action:any) => {
+export default (store: any) => (next: any) => (action: any) => {
     const {promise, type} = action
-    const rest:any = getRest(action)
+    const rest: any = getRest(action)
 
     // 没有 promise 字段不处理
     if (!promise) return next(action)
@@ -37,12 +37,12 @@ export default (store:any) => (next:any) => (action:any) => {
             type: REQUEST
         }))
 
-        return promise.then((req:any) => {
+        return promise.then((req: any) => {
             next(extendRest(rest, {
                 data: req.data, type: SUCCESS
             }))
             return true
-        }).catch((error:any) => {
+        }).catch((error: any) => {
             next(extendRest(rest, {
                 error, type: FAILURE
             }))
@@ -52,12 +52,12 @@ export default (store:any) => (next:any) => (action:any) => {
     } else {
         const result = promise(action.data, action.req)
         if (typeof result.then === 'function') {
-            return result.then((data:any) => {
+            return result.then((data: any) => {
                 next(extendRest(rest, {
                     data: data, type: SUCCESS
                 }))
                 return true
-            }).catch((error:any) => {
+            }).catch((error: any) => {
                 next(extendRest(rest, {
                     error, type: FAILURE
                 }))
